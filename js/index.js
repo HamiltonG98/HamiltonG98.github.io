f8bbea8f5f4ae6bf44c3a6aac98800e5e8f4aa29
Hamilton Garcia <hngr98@gmail.com>
2020-04-05 18:21:08 -0600

    Responsive

---
+++ js/index.js
@@ -0,0 +1,16 @@
+abrirCerrarNav = () => {
+    const nav = document.querySelector('.sidebar');
+
+    nav.classList.toggle('sidebar-active')
+}
+
+/* cerrarNav = () => {
+    const nav = document.querySelector('.sidebar');
+
+} */
+
+/* normal = () => {
+    const nav = document.querySelector('.sidebar');
+    if(screen.width > 480)
+        nav.style.width = '25%';
+} */
